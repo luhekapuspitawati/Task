ce6cecc7a747b8dd291bbeacefe99d317520cf17
zetta-eka-puspitawati <eka.puspitawati@zetta-intern.space>
2024-09-23 17:25:32 +0800

    first commit

---
+++ Task2.js
@@ -0,0 +1,11 @@
+// Step 1: Declare the variables
+let paragraph = 'JavaScript is a programming languange or ,\nin other word, \na means by a \ncomputer is instructed to do things';
+
+// Step 2: Calculate the length of paragraph and assign to paragraphLen
+let paragraphLen = paragraph.length; // This will give the length of the paragraph string
+
+// Step 3: Convert the paragraph to lowercase and assign to paragraphLowercase
+let paragraphLowercase = paragraph.toLowerCase(); // Converts the paragraph to lowercase
+
+// Step 4: Compare the original paragraph with the lowercased version
+console.log(paragraph === paragraphLowercase); // This will log true or false
